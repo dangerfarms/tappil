a9d9cebedc6d976ad4f0c4917540ff1c793cafab
Lewis Taylor <lewis@dangerfarms.com>
2015-02-18 14:37:56 +0000

    Adding admin URL first in urls.py

--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from tappil.activations.views import Activation
 
 urlpatterns = patterns('',
-    url(r'^(?P<code>\w+)/$', Activation.as_view(), name='activation'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(?P<code>\w+)/$', Activation.as_view(), name='activation'),
 )
