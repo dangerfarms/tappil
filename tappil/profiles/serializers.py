ecf0433da1f0819b77a93cf56f091dcfc58e29fe
Lewis Taylor <lewis@dangerfarms.com>
2015-02-20 15:08:31 +0000

    Meta data now flattened

--- tappil/profiles/serializers.py
+++ tappil/profiles/serializers.py
@@ -1,9 +1,15 @@
+import ast
 from rest_framework import serializers
 from tappil.profiles.models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
 
+    meta_data = serializers.SerializerMethodField()
+
     class Meta:
         model = Profile
         depth = 1
+
+    def get_meta_data(self, obj):
+        return ast.literal_eval(obj.meta_data)
