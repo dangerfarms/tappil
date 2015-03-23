44eeb8c373c4b011c7cf7042a5c0646cb70c4dcc
Lewis Taylor <lewis@dangerfarms.com>
2015-03-23 16:45:06 +0000

    More debg

--- project/urls.py
+++ project/urls.py
@@ -2,12 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 
-api_patterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^profile/', include('tappil.profiles.urls')),
-    url(r'^link/', include('tappil.links.urls')),
-)
-
-urlpatterns = patterns(
-    url(r'^v1/', include(api_patterns)),
+urlpatterns = patterns('',
+    url(r'^v1/admin/', include(admin.site.urls)),
+    url(r'^v1/profile/', include('tappil.profiles.urls')),
+    url(r'^v1/link/', include('tappil.links.urls')),
 )
