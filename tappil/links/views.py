aa0caf11ae3184379128c6c927e17d3f10191485
Lewis Taylor <lewis@dangerfarms.com>
2015-04-02 16:36:01 +0100

    Redirecting to play store and apple dependant on device

--- tappil/links/views.py
+++ tappil/links/views.py
@@ -2,13 +2,23 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 from tappil.links.models import Link
-from tappil.links.response import DeepLinkRedirect
 from tappil.profiles.models import Profile
 import user_agents
 
 
+PIN_SEEKERZ_URL = ''
+APP_STORE_URL = ''
+PLAY_STORE_URL = ''
+
+
 class Activation(TemplateView):
     template_name = 'index.html'
+
+    STORE_URLS = {
+        'Android': 'https://play.google.com/store/apps/details?id=com.pinseekerz.www',
+        'iOS': 'https://itunes.apple.com/gb/app/pin-seekerz-golf-world-ranking/id933904752',
+        'default': 'http://www.pinseekerz.com',
+    }
 
     def get_user_agent(self):
         ua_string = self.request.META['HTTP_USER_AGENT']
@@ -17,7 +27,6 @@ class Activation(TemplateView):
 
     def set_profile(self, profile):
         ua_string, user_agent = self.get_user_agent()
-
         profile.user_agent = ua_string
         profile.device_family = user_agent.device.family
         profile.device_os = user_agent.os.family
@@ -28,8 +37,8 @@ class Activation(TemplateView):
         profile.save()
 
     def generate_response(self, profile, link):
-        return HttpResponseRedirect('https://itunes.apple.com/gb/app/pin-seekerz-golf-world-ranking/id933904752')
-        # return self.render_to_response({'profile': profile})
+        redirect_url = self.STORE_URLS.get(profile.device_os, self.STORE_URLS['default'])
+        return HttpResponseRedirect(redirect_url)
 
     def get_link(self):
         link_code = self.kwargs['code']
@@ -41,6 +50,9 @@ class Activation(TemplateView):
 
 
         """
+        user_string, agent = self.get_user_agent()
+        import ipdb; ipdb.set_trace()
+
         link = self.get_link()
 
         ip = request.META['REMOTE_ADDR']
