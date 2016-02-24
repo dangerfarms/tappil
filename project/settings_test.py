acdea7d051948988f5beb743fd2a70a8396408b8
Lewis Taylor <lewis@dangerfarms.com>
2016-02-24 21:27:40 +0000

    Move DATABASE_URL above settings import

--- project/settings_test.py
+++ project/settings_test.py
@@ -1,9 +1,9 @@
+DATABASE_URL = 'postgis://postgres:postgres@db:5432/postgres'
+
 from project.settings import *
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
 ]
-
-DATABASE_URL = 'postgis://postgres:postgres@db:5432/postgres'
 
 SECRET_KEY = 'secret'
