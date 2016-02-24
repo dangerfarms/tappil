e8c7c3566ada39cb85cef3d4ffb2a0bfbe9bd4d8
Lewis Taylor <lewis@dangerfarms.com>
2016-02-24 21:31:00 +0000

    Create config files

--- project/settings_test.py
+++ project/settings_test.py
@@ -1,9 +1,5 @@
-DATABASE_URL = 'postgis://postgres:postgres@db:5432/postgres'
-
 from project.settings import *
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
 ]
-
-SECRET_KEY = 'secret'
