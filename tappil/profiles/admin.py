4bb1ad8476c86f52756aa6d7e52f17bfa0d2ab95
Lewis Taylor <lewis@dangerfarms.com>
2016-03-16 16:34:23 +0000

    #234: Add ip and uuid to search in Admin

--- tappil/profiles/admin.py
+++ tappil/profiles/admin.py
@@ -5,6 +5,7 @@ from tappil.profiles.models import Profile
 class ProfileAdmin(admin.ModelAdmin):
 
     list_display = ('uuid', 'ip', 'device_family', 'device_os', 'device_version', 'user_agent')
+    search_fields = ('uuid', 'ip',)
 
 
 admin.site.register(Profile, ProfileAdmin)
