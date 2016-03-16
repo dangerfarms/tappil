d57319419e5b3f0bcd4e02ed4c1f57f2f30d7cd2
Lewis Taylor <lewis@dangerfarms.com>
2016-03-16 17:51:41 +0000

    #234: Disable logging in tests

--- project/settings_test.py
+++ project/settings_test.py
@@ -1,7 +1,10 @@
 from project.settings import *
+import logging
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
 ]
 
 SECRET_KEY = 'secret'  # Needed for drone
+
+logging.disable(logging.CRITICAL)
